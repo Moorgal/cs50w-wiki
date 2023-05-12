b102438279a7a116aaa1861b0a91fa971d6037c5
Peter Bokor <megaboki@gmail.com>
2023-05-12 23:09:06 +0100

    search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -21,12 +21,15 @@ def search_page(request):
         entries = util.list_entries()
         pages = []
         for entry in entries:
-            if entry.lower() in form.lower():
+            if form.lower() in entry.lower():
                 pages.append(entry) 
         len_pages = len(pages)
         if len_pages == 1:
             return render(request, "encyclopedia/entrypage.html",{
             "entrypage": util.get_entry(pages[0])})
+        elif len_pages > 1:
+            return render(request, "encyclopedia/search_page.html",{
+            "pages": pages})
         else:
             problem = "no cookie in the jar"
             return render(request, "encyclopedia/error.html",{
